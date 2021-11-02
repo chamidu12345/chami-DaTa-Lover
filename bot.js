d79898e4dad57c164a926b433828163396738aa7
chamidu12345 <87266811+chamidu12345@users.noreply.github.com>
2021-11-02 09:41:12 +0530

    Fixed

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ async function whatsAsena () {
     
     const conn = new WAConnection();
     const Session = new StringSession();
-    conn.version = [2, 2119, 6]
+    conn.version = [2, 2140, 12]
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
     var nodb;
